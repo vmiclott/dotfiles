e8be1213cc540f382d43e9854323fc2d4a37400f
vmiclott <victor.miclotte@gmail.com>
2026-04-25 20:44:11 +0200

    Add lazyvim markdown extras

--- nvim/lua/plugins/extras.lua
+++ nvim/lua/plugins/extras.lua
@@ -3,6 +3,7 @@ return {
   { import = "lazyvim.plugins.extras.lang.go" },
   { import = "lazyvim.plugins.extras.lang.java" },
   { import = "lazyvim.plugins.extras.lang.json" },
+  { import = "lazyvim.plugins.extras.lang.markdown" },
   { import = "lazyvim.plugins.extras.lang.python" },
   { import = "lazyvim.plugins.extras.lang.ruby" },
   { import = "lazyvim.plugins.extras.lang.typescript" },
